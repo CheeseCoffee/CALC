1e079356e50ecc82e27f11235ccfb2481b1d7450
GraffT <sanekpanov@gmail.com>
2013-05-06 11:17:47 +0400

    Update Divide.java

--- src/mycalc/Divide.java
+++ src/mycalc/Divide.java
@@ -2,6 +2,7 @@ package mycalc;
 
 public class Divide extends Operation{
 	public float count(float a, float b) {
+		// CPA: а тут обязательно отлавливать исключение?
 		try {
 			return a / b;
 		} catch (ArithmeticException e) {
